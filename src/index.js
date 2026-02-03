bba476db44d39efc8af125e652c5ddd4e300810e
Ad1tyaBhargav <adityabhargav2612@gmail.com>
2026-02-03 23:25:39 +0530

    Mino

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import App from './App';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID 
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={googleClientId}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
